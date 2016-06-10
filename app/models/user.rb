c18a3628adaad8746954a10fe402df66802c7611
Krzysztof Piotrowski <krzysiekp91@gmail.com>
2016-06-10 17:48:08 +0200

    Add validations for User params

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  validates :username, present: true, length: { in: 4..25 }, uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, present: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  
+  has_secure_password
+  validates :password, present: true, length: { minimum: 6 }
 end
