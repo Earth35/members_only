d3127e77c74ee03b67a9c4972a1c30c298554004
Krzysztof Piotrowski <krzysiekp91@gmail.com>
2016-06-10 19:47:09 +0200

    Assign token for new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,18 @@
 class User < ActiveRecord::Base
+  before_create :generate_remember_token
+
   validates :username, presence: true, length: { in: 4..25 }, uniqueness: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+  
+  private
+  
+    def generate_remember_token
+      token = SecureRandom.urlsafe_base64
+      token_digest = Digest::SHA1.hexdigest(token)
+      self.remember_token = token_digest
+    end
 end
