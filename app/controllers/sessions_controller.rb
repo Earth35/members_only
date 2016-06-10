43895e542689e1c8091ea6b99e57ec18e0d09f1f
Krzysztof Piotrowski <krzysiekp91@gmail.com>
2016-06-10 20:56:58 +0200

    Add log_in and log_out helper methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,14 @@ class SessionsController < ApplicationController
   def new
   end
   
-  # def create
-    # user = User.find_by(params[:session][:email].downcase)
-    
-    
-  # end
+  def create
+    user = User.find_by(params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in(user)
+      flash[:success] = "You've successfully logged in"
+      # redirect_to root_path
+    else
+      #re-render
+    end
+  end
 end
