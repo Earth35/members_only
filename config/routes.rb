e45718da80ed054e15feedf39710bbd76c010e89
Krzysztof Piotrowski <krzysiekp91@gmail.com>
2016-06-11 12:35:12 +0200

    Add Posts controller and necessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   
+  root 'posts#index'
+  
+  get 'new_post' => 'posts#new'
+  get 'posts' => 'posts#index'
+
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
