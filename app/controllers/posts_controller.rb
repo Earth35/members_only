c23fba0d717f7181db349e567c8fd61be9cb2b7b
Krzysztof Piotrowski <krzysiekp91@gmail.com>
2016-06-11 13:49:04 +0200

    Update posts#create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,15 @@ class PostsController < ApplicationController
   end
   
   def create
+    @post = Post.new(post_params)
+    @post.user_id = @current_user.id
+    if @post.save
+      flash[:success] = "New post has been successfully created."
+      redirect_to posts_index_path
+    else
+      flash.now[:error] = "Couldn't create new post."
+      render 'new'
+    end
   end
   
   private
@@ -18,5 +27,9 @@ class PostsController < ApplicationController
         flash[:error] = "Please log in."
         redirect_to login_path
       end
+    end
+    
+    def post_params
+      params.require(:post).permit(:title, :body)
     end
 end
